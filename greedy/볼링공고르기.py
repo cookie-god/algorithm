d259511098bf9745c7a1e31aa408353b96b54c65
cookie-god <cookie-god@softsquared.com>
2022-01-05 22:59:44 +0900

    실전문제 3회차 풀이

--- greedy/볼링공고르기.py
+++ greedy/볼링공고르기.py
@@ -12,18 +12,35 @@
 # print(count)
 
 # 답지
+# N, K = map(int, input().split())
+# data = list(map(int, input().split()))
+#
+# array = [0]*11
+#
+# for x in data :
+#     array[x] += 1
+#
+# result = 0
+#
+# for i in range(1, K+1) :
+#     N -= array[i]
+#     result += array[i] * N
+#
+# print(result)
+
+#2번째
 N, K = map(int, input().split())
 data = list(map(int, input().split()))
 
-array = [0]*11
+arr = [0]*11
 
-for x in data :
-    array[x] += 1
+for x in data:
+    arr[x] += 1
 
 result = 0
 
-for i in range(1, K+1) :
-    N -= array[i]
-    result += array[i] * N
+for i in range(1, K+1):
+    N -= arr[i]
+    result += arr[i] * N #같은 숫자가 여러개인 경우, 여러 경우의 수를 계산하기위해 곱함
 
 print(result)
