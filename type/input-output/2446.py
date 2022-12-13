1a2a55be5b87433c9fab89d8c5c66a621b3f9775
cookie <lion0193@gmail.com>
2022-12-13 11:13:48 +0900

    2446번 해결

---
+++ type/input-output/2446.py
@@ -0,0 +1,9 @@
+import sys
+
+N = int(sys.stdin.readline().strip())
+
+for i in range(N, 0, -1):
+    print(' ' * (N - i) + '*' * (2 * i - 1))
+
+for i in range(2, N + 1):
+    print(' ' * (N - i) + '*' * (2 * i - 1))
