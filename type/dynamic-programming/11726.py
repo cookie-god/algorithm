b6f44bccf1280854217e49b47444279b7c9e9f0c
cookie <lion0193@gmail.com>
2022-12-13 17:59:34 +0900

    11726번 해결

---
+++ type/dynamic-programming/11726.py
@@ -0,0 +1,13 @@
+import sys
+
+N = int(sys.stdin.readline().strip())
+dp = [0 for _ in range(N + 2)]
+
+dp[1] = 1
+dp[2] = 2
+
+for i in range(3, N + 2):
+    dp[i] = dp[i - 1] + dp[i - 2]  # 뒤에 길이가 1인 타일이 붙는 경우, 길이가 2인 타일이 붙는 경우
+
+print(dp[N] % 10007)
+
