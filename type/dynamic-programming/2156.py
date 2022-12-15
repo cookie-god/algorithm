6ceeb4aae649b20a14cf8b934d9881a114b17fb0
cookie <lion0193@gmail.com>
2022-12-15 17:33:45 +0900

    2156번 해결

--- type/dynamic-programming/2156.py
+++ type/dynamic-programming/2156.py
@@ -1,25 +1,20 @@
 import sys
 
 n = int(sys.stdin.readline().strip())
-arr = []
-dp = [0 for _ in range(n)]
+arr = [0]
+dp = [0 for _ in range(n + 1)]  # dp[i]는 i번째 포도주 잔에서 최대로 포도주를 먹은 양을 담는 배열
 for _ in range(n):
     arr.append(int(sys.stdin.readline().strip()))
 
-for i in range(3, n):
-    dp[i] += max(arr[i - 1] + arr[i - 3], arr[i - 2] + arr[i - 3])
+dp[1] = arr[1]
 
-print(dp[n-1])
-# for i in range(T):
-#     n = int(sys.stdin.readline().strip())
-#     dp = [list(map(int, sys.stdin.readline().split())) for _ in range(2)]
-#
-#     if n > 1:
-#         dp[0][1] += dp[1][0]  # 맨 처음 대각선에 있는 점수 더하기
-#         dp[1][1] += dp[0][0]  # 맨 처음 대각선에 있는 점수 더하기
-#
-#         for j in range(2, n):
-#             dp[0][j] += max(dp[1][j - 2], dp[1][j - 1])  # 왼쪽 대각선에 있는 것과 왼쪽 대각선 옆에 있는 것이랑 비교
-#             dp[1][j] += max(dp[0][j - 2], dp[0][j - 1])  # 왼쪽 대각선에 있는 것과 왼쪽 대각선 옆에 있는 것이랑 비교
-#
-#     print(max(dp[0][n - 1], dp[1][n - 1]))
+if n >= 2:  # 1이상부터 되므로
+    dp[2] = arr[1] + arr[2]
+
+for i in range(3, n + 1):
+    dp[i] = max(arr[i] + arr[i-1] + dp[i - 3], arr[i] + dp[i - 2], dp[i - 1])
+    #  현재 포도주양, 전 포도주양, 처음부터 전전전 포도주까지 먹은 양중에 가장 많이 먹을 수 있는 방법으로 먹은 포도주(dp[전전전])를 먹는 경우, 즉 전전 포도주를 먹지 않는 경우
+    #  두번째 경우는 현재 포도주양, 처음부터 전전전 포도주까지 먹은 양중에 가장 많이 먹을 수 방법으로 먹은 포도주(dp[전전])를 먹는 경우,즉 전 포도주를 안먹는 경우
+    #  세번째 경우는 처음부터 전 포도주까지 먹은 양중에 가장 많이 먹을 수 방법으로 먹은 포도주(dp[전])를 먹는 경우, 즉 현재 포도주를 안먹는 경우
+
+print(dp[n])
